738ff00c484a2e92fc15b3246bf4ddc88a68c623
Vladimir Dmitriev <svami.vd@gmail.com>
2024-07-08 21:24:37 +0400

    Initial Commit

---
+++ Delta/DeltaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeltaApp.swift
+//  Delta
+//
+//  Created by Vladimir Dmitriev on 08.07.24.
+//
+
+import SwiftUI
+
+@main
+struct DeltaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
