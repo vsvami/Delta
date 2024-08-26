b20b47e099fed80c12be9332202d3d4f31782768
Vladimir Dmitriev <svami.vd@gmail.com>
2024-08-26 22:15:41 +0400

    MyBalanceView SetupUI

--- Delta/CustomViews/MyBalanceView.swift
+++ Delta/CustomViews/MyBalanceView.swift
@@ -6,10 +6,35 @@
 //
 
 import SwiftUI
+import UISystem
 
 struct MyBalanceView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Мой баланс")
+                    .font(.system(size: 12))
+                Spacer()
+                Text("₽ 120000")
+                    .font(.title.bold())
+            }
+            .minimumScaleFactor(0.5)
+            .lineLimit(1)
+            
+            Spacer()
+            
+            Image("person")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(.circle)
+                .layoutPriority(1)
+                .frame(maxHeight: .infinity)
+        }
+        .frame(height: 55)
+        .padding()
+        .background(.white)
+        .cornerRadius(16)
+        .shadow(color: Color.gray.opacity(0.2), radius: 16)
     }
 }
 
