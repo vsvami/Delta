d3e2feadc117f07f56bd038895f99e2b384f1c2d
Vladimir Dmitriev <svami.vd@gmail.com>
2024-08-28 20:48:27 +0400

    GoalCategoryCardView InvestmentCategoryCardView SetupUI

--- Delta/Modifiers/ComponentBackgroundModifier.swift
+++ Delta/Modifiers/ComponentBackgroundModifier.swift
@@ -15,7 +15,7 @@ struct ComponentBackgroundModifier: ViewModifier {
             .padding()
             .background(color)
             .cornerRadius(16)
-            .shadow(color: Color.gray.opacity(0.3), radius: 16)
+            .shadow(color: Color.gray.opacity(0.2), radius: 16)
     }
 }
 
