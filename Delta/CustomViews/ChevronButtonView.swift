6575323d7b2397c5c6e7f0d6513f252baa3cd075
Vladimir Dmitriev <svami.vd@gmail.com>
2024-08-30 02:01:24 +0400

    ButtonViews Setup UI

---
+++ Delta/CustomViews/ChevronButtonView.swift
@@ -0,0 +1,34 @@
+//
+//  ChevronButtonView.swift
+//  Delta
+//
+//  Created by Vladimir Dmitriev on 30.08.24.
+//
+
+import SwiftUI
+
+struct ChevronButtonView: View {
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: {}) {
+        Label("See all", systemImage: "chevron.right.circle.fill")
+            .font(.subheadline)
+            .labelStyle(IconTrailingLabelStyle())
+            .foregroundStyle(.appBlack)
+        }
+    }
+}
+
+struct IconTrailingLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+#Preview {
+    ChevronButtonView(action: {})
+}
