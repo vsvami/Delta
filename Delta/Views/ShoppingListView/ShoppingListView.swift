3106df216bab86e17cee2cd3ae87bc41266e309f
Alex Petrenko <alex.pt@ya.ru>
2024-10-09 21:56:06 +0300

    add bottom padding for List

--- Delta/Views/ShoppingListView/ShoppingListView.swift
+++ Delta/Views/ShoppingListView/ShoppingListView.swift
@@ -220,6 +220,7 @@ struct ShoppingListView: View {
                         }
                     }
                 }
+                .padding(.bottom, 75)
                 .listSectionSpacing(.compact)
                 .navigationTitle("Shopping List")
                 .toolbar {
