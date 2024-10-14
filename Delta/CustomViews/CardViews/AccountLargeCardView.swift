950c552bd8a691f76d964872e62bff95edb6a6ac
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-10-14 12:48:53 +0300

    change background in Account Large Card

--- Delta/CustomViews/CardViews/AccountLargeCardView.swift
+++ Delta/CustomViews/CardViews/AccountLargeCardView.swift
@@ -24,7 +24,7 @@ struct AccountLargeCardView: View {
                         .font(.heading2())
                         .padding(.bottom, 2)
                     
-                    Text(Double(amount)?.formattedAmount(for: currency) ?? "-10")
+                    Text(Double(amount)?.formattedAmount(for: currency) ?? "1000")
                         .font(.subheading2())
                 }
                 .foregroundStyle(color == AppGradient.appBlack.name ? .appWhite : .black)
@@ -38,10 +38,9 @@ struct AccountLargeCardView: View {
                 .foregroundStyle(color == AppGradient.appBlack.name ? .appWhite : .black)
         }
         .padding()
-        .componentBackground(
-            color: color,
-            size: CGSize(width: size.width, height: size.height)
-        )
+        .frame(width: size.width, height: size.height)
+        .background(AppGradient.getColor(from: color)?.value)
+        .cornerRadius(18)
     }
 }
 
